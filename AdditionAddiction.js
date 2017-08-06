944cb0e8dbae7d304aa046f7df07c3d392598fa1
QimanWang <wangqiman0111@gmail.com>
2017-08-06 14:01:09 -0400

    Add files via upload

---
+++ AdditionAddiction.js
@@ -0,0 +1,115 @@
+/* MoMath Math Square Behavior
+ *
+ *        Title: Simple Math
+ *  Description: Sum Solution
+ * Scheduler ID: xx
+ *    Framework: none
+ *       Author: xhn & bob
+ *      Created: 2017-08-05
+ *       Status: dev
+ */
+
+
+import P5Behavior from 'p5beh';
+const pb = new P5Behavior();
+import * as Sensor from 'sensors';
+
+var problem;
+var s;
+var step=true;
+var num1;
+var num2;
+var answer;
+var axx;
+var ayy;
+var problemStatement;
+var score =0;
+pb.setup = function (p) {
+    /* this == pb.p5 == p */
+    /* P5Behavior already calls createCanvas for us */
+    // setup here...
+
+    s = 1;
+};
+
+
+pb.draw = function (floor,p) {
+    this.textSize(40);
+    var j = 0;
+
+    while (step) {
+        this.clear();
+//problem statement
+        var p1 = Math.random()*10;
+        var p2 = Math.random()*10;
+
+        num1 = p1.toFixed();
+        num2 = p2.toFixed();
+        answer = +num1 + +num2;
+        console.log(answer);
+        var prob1 = num1.toString();
+        var prob2 = num2.toString();
+        problemStatement = prob1.concat(" + ",prob2, " = ??");
+        console.log(problemStatement);
+
+        this.fill(256, 256, 256);
+
+        while(j<10) {
+
+            var s = Math.random()*99;
+            var yy = Math.random()*576;
+            var xx = Math.random()*576;
+            this.text(problemStatement,150,32);
+            this.text(s.toFixed(), yy.toFixed(), xx.toFixed());
+            this.fill(256, 256, 256);
+            j++;
+        }
+//print answer on board
+        ayy = Math.random()*576;
+        axx = Math.random()*576;
+        this.text(answer,axx.toFixed(), ayy.toFixed());
+        var scoreStatement = "score: ".concat(score);
+        this.text(scoreStatement,400,32);
+
+
+
+        step = false;
+        console.log('printed board')
+    }
+    for(var i = new Sensor.Index(); i; i = i.incr()){
+        if (floor.sensors.get(i)){
+            console.log('This sensor is enabled');
+            console.log(i.x*8,i.y*8);
+            console.log(axx,ayy);
+            //
+            console.log(i.x*8 + 30);
+            console.log( i.x*8 - 30);
+            console.log( i.y*8 + 30);
+            console.log(i.y*8 - 30);
+
+            if (((axx < i.x*8 + 30) || (axx > i.x*8 - 30)) && ((ayy < i.y*8 + 30) || (ayy > i.y*8 - 30)) ){
+
+                step = true;
+                console.log(step);
+                score+=1;
+                return;
+            } else {
+
+                step = false;
+                console.log(step);
+            }
+
+        }
+    }
+
+
+};
+
+export const behavior = {
+    title: "xhn",
+    init: pb.init.bind(pb),
+    frameRate: 'sensor',
+    render: pb.render.bind(pb),
+    numGhosts: 1
+};
+export default behavior;
